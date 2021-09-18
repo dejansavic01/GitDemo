6ee4e62296c8f86c0163469923967f2779829006
Dejan <savic.dejan01@gmail.com>
2021-09-18 13:18:12 +0200

    first commit

---
+++ src/test/java/qaclickacademy/MavenJava/SeleniumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	//testing xml file from maven
+	//how to execute testcases frome Test folder-mvn test
+	@Test
+	public void BrowserAutomation()
+	
+	{
+		System.out.println("BrowserAutomation");
+	}
+	@Test
+	public void ElementsUI()
+	{
+		
+		System.out.println("ElementsUI");
+	}
+
+}
